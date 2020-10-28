333ab3cb67be3e6579c8ad25d453b2bd6ed9ba27
github_chaitanyamuvvala <muvvalachaitanya05@gmail.com>
2020-10-28 20:12:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React , { Component } from 'react'
+import classes from './App.css';
+
+import Login from './components/login/login'
+// import Glogin from "./components/googlelogin/googleLogin"
+
+//AIzaSyC2PCUOjSlPxdGwybxJ90CzjnKCG27hOWA - api key
+class App extends Component {
+  render() {
+    return (
+      <div className = {classes.App} >
+        
+      
+        <div className = {classes.vectorOne}>
+        
+        <svg className = {classes.svgone} viewBox="0 0 654 608" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M653.5 1C653.5 1 484.949 48.1897 408 122.5C358.44 170.361 365.803 230.79 307.5 267.5C216.888 324.552 139.6 168.67 64 244.5C4.98332 303.696 137.736 411.621 127.5 496C114.035 607 0 607 0 607" stroke="#0C4AB7" strokeWidth="0.4"/>
+        </svg>
+
+        </div>
+      
+        <div className = {classes.vectorTwo}>
+        <svg className = {classes.svgTwo} viewBox="0 0 387 491" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M387.5 251C387.5 251 306.75 226.102 298 184C289.265 141.968 362.967 91.4346 346 52C322.134 -3.47043 252.899 -9.08173 196.5 12.5C115.275 43.5819 215.174 171.803 151 230.5C107.802 270.012 45.1142 233.536 9.50016 280C-54.8859 364.001 241.05 275.777 316.5 350C378 410.5 279.5 492 279.5 492" stroke="#0C4AB7" strokeWidth="0.4"/>
+        </svg>        
+        </div>
+
+        
+        <div className = {classes.ellipse}>
+        <svg width="20" height="23" viewBox="0 0 20 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 6.99999C19 11.9706 10.4706 22.5 5.5 22.5C0.529437 22.5 0.5 20.4705 0.5 15.5C0.5 10.5294 0.529437 4.49998 5.5 4.49998C10.4706 4.49998 17 -4.5 19 6.99999Z" stroke="#0C4AB7" strokeWidth="0.4"/>
+        </svg>
+        </div>
+
+        <div className = {classes.ellipseTwo}>
+        <svg width="27" height="25" viewBox="0 0 27 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M26.5 14C26.5 18.9706 20.9706 24.5 16 24.5C11.0294 24.5 0.5 16.4706 0.5 11.5C0.5 6.52943 4.5 0.5 13.5 0.5C21.5 0.5 26.5 9.02943 26.5 14Z" stroke="#0C4AB7" strokeWidth="0.4"/>
+        </svg>
+        </div>
+        
+        <div className = {classes.ellipseThree}>
+          <svg width="21" height="34" viewBox="0 0 21 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 20C20 28 9.97058 33 5.00001 33C0.0294499 33 2.00001 24.9706 2.00001 20C2.00001 15.0294 -0.970551 1 4.00001 1C8.97058 1 20 9.5 20 20Z" stroke="#0C4AB7" strokeWidth="0.4"/>
+          </svg>        
+        </div>
+
+        <div className =  {classes.ellipseFour}>
+          <svg width="13" height="20" viewBox="0 0 13 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 10C10 14.9706 14.9706 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C13 1 10 5.02944 10 10Z" stroke="#0C4AB7" strokeWidth="0.4"/>
+          </svg>        
+        </div>
+        
+        <div className = {classes.ellipseFive}>
+        <svg width="62" height="36" viewBox="0 0 62 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M61 4.5C61 16.098 33.098 35.4999 21.5 35.4999C9.90202 35.4999 0.5 26.0979 0.5 14.4999C0.5 2.90194 9.90202 10.4999 21.5 10.4999C33.098 10.4999 61 -7.09798 61 4.5Z" stroke="#0C4AB7" strokeWidth="0.4"/>
+        </svg>
+        </div>
+
+        <div className = {classes.ellipseSix}>
+        <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18.5 10.5C18.5 15.4705 12.4706 13.5 7.5 13.5C2.52944 13.5 0.5 15.4705 0.5 10.5C0.5 5.52942 2.52944 0.5 7.5 0.5C12.4706 0.5 18.5 5.52942 18.5 10.5Z" stroke="#0C4AB7" strokeWidth="0.4"/>
+        </svg>
+        
+        </div>
+    
+        <Login />
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ src/components/login/login.js
@@ -0,0 +1,141 @@
+import React , { Component } from "react"
+import classes from "./login.css"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+
+
+
+
+class Login extends Component {
+
+    state = {
+        isSignedIn: null,
+    };
+
+    insertGapiScript() {
+        const script = document.createElement('script')
+        script.src = 'https://apis.google.com/js/platform.js'
+        script.onload = () => {
+            this.initializeGoogleSignIn()
+        }
+        document.body.appendChild(script)
+    }
+
+    initializeGoogleSignIn() {
+        this.setState({
+            isSignedIn: true
+        })
+        window.gapi.load('auth2', () => {
+            window.gapi.auth2.init({
+                client_id: '958511751255-tq0dgsvsilr2na11hiih0tmi0d3t3nig.apps.googleusercontent.com',
+                cookie_policy: 'single_host_origin'
+            }).then(() => {
+                const authInstance = window.gapi.auth2.getAuthInstance()
+                const isSignedIn = authInstance.isSignedIn.get()
+                this.setState({isSignedIn})
+                authInstance.isSignedIn.listen(isSignedIn => {
+                    this.setState({isSignedIn})
+                })
+            })
+            console.log('API inited')
+        })
+       
+    }
+
+    ifUserSignedIn(Component) {
+        if (this.state.isSignedIn == null) {
+            return (
+                <p></p>
+            )
+        }
+        return this.state.isSignedIn ?
+               <Component />: <LoginPage />
+    }
+    componentDidMount() {
+        console.log('Loading....!!')
+        this.insertGapiScript();   
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path = "/" render = {()=>this.ifUserSignedIn(Dashboard)}></Route>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+}
+
+
+class LoginPage extends Component {
+    componentDidMount() {
+        window.gapi.load('signin2',() => {
+            window.gapi.signin2.render('login-button')
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className = {classes.Rectangle}>
+                    <img className = {classes.img} src =  "/image.png" alt = "" />
+                    <p className = {classes.para}>Sign in with Google</p>
+                    <input className  = {classes.email} type = "text" placeholder = "Email"/>
+                    <input className  = {classes.password} type = "text" placeholder = "Password" />
+                    <div id = "login-button" className =  {classes.button} style = {{width: '220px'}}></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+class Dashboard extends Component {
+
+    componentDidMount() {
+        window.gapi.load('auth2', function() {
+            window.gapi.auth2.init({
+                client_id: "958511751255-tq0dgsvsilr2na11hiih0tmi0d3t3nig.apps.googleusercontent.com"
+            })
+            console.log('init()')
+        })
+     
+    }
+    
+    signOut() {
+        var auth2 = window.gapi.auth2.getAuthInstance()
+        // auth2.init()
+        auth2.signOut().then(() => {
+            // auth2.disconnect()
+            document.location.reload()
+        })
+        console.log('iuehfihsd')
+    }
+    
+    render() {
+
+        const authInstance = window.gapi.auth2.getAuthInstance()
+        const currUser = authInstance.currentUser.get()
+        const profile = currUser.getBasicProfile()
+        const email = profile.getEmail()
+        const name = profile.getName()
+        const imageUrl = profile.getImageUrl()
+        // console.log(authInstance)
+        return (
+            <div>{email}
+            <br />
+                <img src={imageUrl} alt= "null"/>
+                <p>{name}</p>
+
+                <button onClick = {this.signOut()}>Sign Out</button>
+                
+            </div>
+            
+        )
+    
+    }
+}
+
+
+
+export default Login
